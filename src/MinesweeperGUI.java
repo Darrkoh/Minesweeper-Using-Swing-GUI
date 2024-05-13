87bf6515396095ba9ca0a2d967d668243e07829d
Darrkoh <157045831+Darrkoh@users.noreply.github.com>
2024-05-13 20:39:53 +0100

    Added Solution (The amount of bombs around each button + Marking each button with a bomb)

--- src/MinesweeperGUI.java
+++ src/MinesweeperGUI.java
@@ -15,6 +15,7 @@ public class MinesweeperGUI implements ActionListener {
     JPanel textPanel;
     JButton[][] buttons; // 'JButton[][]' Creates a 2D array of buttons, so we can have a grid
     JLabel textLabel;
+    int [][] solution; //2D Array which will check each button and add the number of bombs around each button to a variable
 
 
     //Other Properties
@@ -56,6 +57,8 @@ public class MinesweeperGUI implements ActionListener {
         textLabel.setFont(new Font("MV Bold", Font.BOLD, 20));
         textLabel.setForeground(Color.WHITE);
         textLabel.setText("There are " + bombs + " Bombs");
+
+        solution = new int[gridSize][gridSize]; //Array Size is the size of the grid
 
         //Buttons Setup\\
         buttons = new JButton[gridSize][gridSize];
@@ -109,9 +112,75 @@ public class MinesweeperGUI implements ActionListener {
         for (int i=0; i < bombs; i++) {
             System.out.println("The X axis of Bomb " + i + " is " + xPosition.get(i) + " and the Y axis is " + yPosition.get(i));
         }
-
+        getSolution(); //Will get the solution of each tile
     }
-                    ////////////WHEN AN ACTION IS PERFORMED\\\\\\\\\\\\
+
+
+
+            //////////For Bombs Around Tile Clicked\\\\\\\\\\
+    public void getSolution() {
+        for (int y = 0; y < solution.length; y++)
+        {
+            for (int x = 0; x < solution[0].length; x++) //Goes through 9x9 arraylist of solution variable
+            {
+                boolean changed = false; //This condition will be changed to true if the position has a bomb and has been modified
+                int bombsAround = 0;
+
+                for (int i = 0; i < xPosition.size(); i++) {
+                    if (x == xPosition.get(i) && y == yPosition.get(i)) {
+                        solution[y][x] = gridSize + 1; //If a bomb is present, the position will have 1 added to it, making 10, along with the changed condition being changed
+                        changed = true;
+                    }
+                }
+                if (!changed) {
+                    for (int i = 0; i < xPosition.size(); i++) {
+                        if (x - 1 == xPosition.get(i) && y == yPosition.get(i)) //Checks if the buttons Left of the one being checked has a bomb or not
+                        {
+                            bombsAround++; //Adds 1 to bombs around counter
+                        }
+                        if (x - 1 == xPosition.get(i) && y - 1 == yPosition.get(i)) //Top Left of one being checked
+                        {
+                            bombsAround++; //Adds 1 to bombs around counter
+                        }
+                        if (x == xPosition.get(i) && y - 1 == yPosition.get(i)) //Above one being checked
+                        {
+                            bombsAround++; //Adds 1 to bombs around counter
+                        }
+                        if (x + 1 == xPosition.get(i) && y - 1 == yPosition.get(i)) //Top Right of one being checked
+                        {
+                            bombsAround++; //Adds 1 to bombs around counter
+                        }
+                        if (x + 1 == xPosition.get(i) && y == yPosition.get(i)) //Right of one being checked
+                        {
+                            bombsAround++; //Adds 1 to bombs around counter
+                        }
+                        if (x + 1 == xPosition.get(i) && y - 1 == yPosition.get(i)) //Bottom Right of one being checked
+                        {
+                            bombsAround++; //Adds 1 to bombs around counter
+                        }
+                        if (x == xPosition.get(i) && y - 1 == yPosition.get(i)) //Bottom of one being checked
+                        {
+                            bombsAround++; //Adds 1 to bombs around counter
+                        }
+                        if (x - 1 == xPosition.get(i) && y - 1 == yPosition.get(i)) //Bottom Left of one being checked
+                        {
+                            bombsAround++; //Adds 1 to bombs around counter
+                        }
+                    }
+                    solution[y][x] = bombsAround; //Number of bombs around tile being checked is saved
+                }
+            }
+        }
+        for (int i = 0; i < solution.length; i++)
+        {
+            for (int j = 0; j < solution[0].length; j++)
+            {
+                System.out.print(solution[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+                    ////////////WHEN AN ACTION IS PERFORMED (Tile Is Clicked)\\\\\\\\\\\\
     @Override
     public void actionPerformed(ActionEvent e)
     {
